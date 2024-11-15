ae28945a095987750ffb35120cbdae003dfd4e5d
Shreyash Naik <110555861+shreyashh01@users.noreply.github.com>
2024-11-15 12:11:32 +0530

    Update settings.py

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = 'django-insecure-(&09i&#-1sjydgh5tlc93p2mbkfj#y5-im@h87*g-vho+w%^i@'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes',
-    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
